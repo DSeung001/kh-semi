c2b62dc4d64bee38623cde47262be3d50a954c4f
지승렬 <seungryeol156@gmail.com>
2026-07-24 10:00:35 +0900

    refactor: jsp 링크 연결 및 프로젝트명 변경

---
+++ zzanmat-tour/src/main/java/com/zzanmat/tour/main/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.zzanmat.tour.main.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        //log.debug("ZzanmatTour home view requested");
+        return "main/index";
+    }
+
+}
